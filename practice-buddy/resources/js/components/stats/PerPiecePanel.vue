e12c71fdfdba910a5cb7b01c4edefdb4322d394a
InesDennehy <inesdennehy@gmail.com>
2020-08-04 18:15:04 -0300

    checkpoint

--- practice-buddy/resources/js/components/stats/PerPiecePanel.vue
+++ practice-buddy/resources/js/components/stats/PerPiecePanel.vue
@@ -19,6 +19,7 @@
                         <h4 class="text-center">{{category}}</h4>
                     </div>
                 </div>
+
                 <a class="carousel-control-prev" @click="changeCategoryLeft()" href="#carouselExampleControls" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                     <span class="sr-only">Previous</span>
